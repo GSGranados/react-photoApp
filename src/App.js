f75f2fd582cf1e03507afbd4e3a63d521d196608
GSGranados <stevengranados0211@gmail.com>
2020-02-18 22:21:06 -0600

    React Router DOM added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import InputForm from "./Components/InputForm/InputForm";
 import "./App.css";
+//import ImageList from './Components/ImageList/ImageList';
 
 const API_KEY="15067697-a347138b0dcbd88a15f281e67";
 
@@ -18,34 +19,26 @@ class App extends Component {
 
     const input = e.target.elements.imageSearch.value;
 
-    const response = await fetch(`https://pixabay.com/api/videos/?key=${API_KEY}&q=${input}`);
-
+    const response = await fetch(`https://pixabay.com/api/videos/?key=${API_KEY}&q=${input}&image_type=photo`);
     const result = await response.json();
-
     console.log(result);
-
     this.setState({
       images: result.hits
     })
-
-
   }
- 
-  componentDidMount() {}
-
   render() {
     return (
-      <div>
+      <>
         <div className="App">
           <header className="App-header">
             <p>Image Search.</p>
           </header>
           <InputForm handleRequest={this.handleRequest}></InputForm>
         </div>
-      </div>
+      </>
     );
   }
-}
+};
 
 
 export default App;
---
+++ src/Components/ImageList/ImageList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ImageList.css";
+
+const ImageList = ({ data }) => {
+    return (
+      <>
+      </>
+    );
+  };
+export default ImageList;
